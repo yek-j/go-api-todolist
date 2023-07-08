9c8d98088106379ae8e8601d416fbc5810f2729f
JeonYekyeong <jeonyk219@gmail.com>
2023-07-08 15:55:02 +0900

    모듈명 변경

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	signup "to-do-list/api/user"
+
 	"github.com/gin-gonic/gin"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	// 테스트 
 	router := gin.Default()
+	router.POST("/user/signup", signup.Signup)
+
 	router.Run("localhost:8080")
+
 }
