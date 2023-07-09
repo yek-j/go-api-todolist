02d2ad208c0709bba6a558e732e1c6139cebe374
JeonYekyeong <jeonyk219@gmail.com>
2023-07-09 16:38:09 +0900

    signup handler

--- api/user/signup.go
+++ api/user/signup.go
@@ -1,7 +1,40 @@
 package signup
 
-import "github.com/gin-gonic/gin"
+import (
+	connector "to-do-list/db"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 새 사용자
+type newUser struct {
+	Name 	string 	`form:"name"`
+	Email 	string 	`form:"email"`
+	Password	string	`form:"password"`
+}
 
 func Signup(c *gin.Context) {
+	var user newUser 
+	user.Name = c.PostForm("name")
+	user.Email = c.PostForm("email")
+	user.Password = c.PostForm("password")
+
+	db, ctx := connector.Connector()
+	_ , err := db.User.
+		Create().
+		SetName(user.Name).
+		SetEmail(user.Email).
+		SetPassword(user.Password).
+		SetMemo("").
+		Save(ctx) 
+
+	if err != nil {
+		panic(err)
+	} 
+
+	defer db.Close();
+	if err := db.Schema.Create(ctx); err != nil {
+		panic(err)
+    }
 
 }
