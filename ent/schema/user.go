0cd78e409a6d9bfb5e1de55800d7e36192c787b7
JeonYekyeong <jeonyk219@gmail.com>
2023-07-09 15:48:52 +0900

    user 스키마

---
+++ ent/schema/user.go
@@ -0,0 +1,28 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New),
+		field.String("name").NotEmpty(),
+		field.String("email").Unique().NotEmpty(),
+		field.String("password").Sensitive().NotEmpty(),
+		field.String("memo"),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
