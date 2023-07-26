4f1d995157cfc77fc157fdff633a42df40f6df41
JeonYekyeong <jeonyk219@gmail.com>
2023-07-26 12:12:19 +0900

    jwt token 추가

--- api/user/signin.go
+++ api/user/signin.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 	connector "to-do-list/db"
 	"to-do-list/ent/user"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"github.com/spf13/viper"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -35,7 +38,7 @@ func Signin(c *gin.Context) {
 
 	// email check
 	if len(user) == 0 {
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"signin": "faile",
 			"message": "로그인 실패",
 		})
@@ -44,20 +47,61 @@ func Signin(c *gin.Context) {
 
 	// password check 
 	if !HashCheck(password, user[0].Password) {
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"signin": "faile",
 			"message": "로그인 실패",
 		})
 	} else {
-		c.JSON(http.StatusOK, gin.H{
-			"signin": "success",
-			"message": "로그인 성공",
-		})
+		
+		// 토큰 발생 
+		token, err := GenerateToken(user[0].Name)
+		fmt.Println(token)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"signin": "faile",
+				"error": err,
+				"message": "로그인 실패",
+			})	
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"signin": "success",
+				"token": token,
+				"message": "로그인 성공",
+			})
+		}
 	}
-
 }
 
 func HashCheck(password, hash string) bool {
 	check := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return check == nil
+}
+
+func GenerateToken(name string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+		
+	claims := token.Claims.(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(30 * time.Minute)
+	claims["authorized"] = true
+	claims["user"] = name
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath("config")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		return "", err
+	}
+
+	secretkey := viper.GetString("secretkey")
+
+	result, err := token.SignedString([]byte(secretkey))
+
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
 }
