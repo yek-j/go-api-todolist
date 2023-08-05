94fb79e200dad5eba3acb257caca1f4e0b2b045a
JeonYekyeong <jeonyk219@gmail.com>
2023-08-05 13:42:17 +0900

    todo 스키마

---
+++ ent/schema/todo.go
@@ -0,0 +1,31 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// Todo holds the schema definition for the Todo entity.
+type Todo struct {
+	ent.Schema
+}
+
+// Fields of the Todo.
+func (Todo) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New),
+		field.String("content").NotEmpty(),
+		field.Bool("check").Default(false),
+	}
+}
+
+// Edges of the Todo.
+func (Todo) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("user", User.Type).
+			Ref("todos").
+			Unique(),
+	}
+}
