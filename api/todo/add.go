eb3037be5abe7d66dfab4b4486d806c6df171a4b
JeonYekyeong <jeonyk219@gmail.com>
2023-08-05 13:44:31 +0900

    todo 추가 기능

---
+++ api/todo/add.go
@@ -0,0 +1,66 @@
+package todo
+
+import (
+	"net/http"
+	"strconv"
+	"to-do-list/api/common"
+	connector "to-do-list/db"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type newTodo struct {
+	Content string 
+	Check bool 
+	UserID string 
+}
+
+func Addtodo(c *gin.Context) {
+	var todo newTodo 
+	todo.Content = c.PostForm("content")
+	todo.Check, _ = strconv.ParseBool(c.PostForm("check"))
+	todo.UserID = c.PostForm("userid")
+
+	// token 확인
+	token := c.GetHeader("Authorization")
+	verifiedClaims, err :=common.ValidateToken(token)
+
+	if verifiedClaims == nil {
+		c.JSON(http.StatusNonAuthoritativeInfo, gin.H{
+			"add": "faile",
+			"error": err,
+			"message": "로그인이 필요합니다.",
+		})	
+		return 
+	}
+
+	userID, err := uuid.Parse(verifiedClaims.UserID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Addtodo uuid err",
+		})
+		return
+	}
+
+	db, ctx := connector.Connector()
+	_ , err = db.Todo.
+		Create().
+		SetContent(todo.Content).
+		SetCheck(todo.Check).
+		SetUserID(userID).
+		Save(ctx)
+	
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Addtodo save err",
+		})
+		return
+	} 	
+
+	defer db.Close()
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
